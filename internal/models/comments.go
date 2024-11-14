a20bd51b99be88d3159bb9e5c21e1b8cfba3e883
Shiradil <shirbaev04@mail.ru>
2024-11-14 12:50:28 +0500

    comments, first step

--- internal/models/comments.go
+++ internal/models/comments.go
@@ -1,4 +1,35 @@
 package models
 
+import "database/sql"
+
+type CommentsModelInterface interface {
+
+}
+
 type Comments struct {
+	ID     uint
+	PostID uint
+	UserID uint
+	Text string
+}
+
+type CommentsModel struct {
+	DB *sql.DB
+}
+
+func (m *CommentsModel) Insert(postID uint, userID uint, text string) (uint, error) {
+	stmt := `INSERT INTO Comments (post_id, user_id, text)
+	VALUES (?, ?, ?)`
+
+	result, err := m.DB.Exec(stmt, postID, userID, text)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return uint(id), nil
 }
